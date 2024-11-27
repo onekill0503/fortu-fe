a90f0800b765dc42cbcf4a32ebb4196a933148a2
AdityaWisnuDev <wisnu.barata111@gmail.com>
2024-11-28 00:37:54 +0700

    update for rejoin

--- src/components/customs/SwapCard.tsx
+++ src/components/customs/SwapCard.tsx
@@ -47,6 +47,7 @@ const SwapCard = () => {
   const [usdeBalance, setUsdeBalance] = useState(0);
   const [approveAmount, setApproveAmount] = useState(BigInt(0)); // Approve amount for USDe
   const [bridgeFee, setBridgeFee] = useState("0");
+  const [rejoinBatch, setRejoinBatch] = useState(true);
 
   let TP = BigInt(0);
   let USDE_BALANCE = {
@@ -63,6 +64,16 @@ const SwapCard = () => {
     nativeFee: BigInt(0),
   };
   let composedMessage: string = "0x";
+  let activeFunds = Array<bigint>();
+  let currentBatch = BigInt(0);
+
+  const SC_AF = useReadContract({
+    address: baseSepoliaContract.FORTU_POOL.address as `0x${string}`,
+    abi: baseSepoliaContract.FORTU_POOL.abi,
+    functionName: "activeFunds",
+    args: [account.address],
+    chainId: baseSepolia.id,
+  })  
 
   const SC_TP = useReadContract({
     address: baseSepoliaContract.FORTU_POOL.address as `0x${string}`,
@@ -148,6 +159,9 @@ const SwapCard = () => {
     chainId: baseSepolia.id,
   });
 
+  activeFunds = (SC_AF.data as bigint[]) ?? BigInt(0);
+  currentBatch = (SC_CB.data as bigint) ?? BigInt(0);
+
   TP = (SC_TP.data as bigint) ?? BigInt(0);
   USDE_BALANCE.base = (SC_USDE_BALANCE_BASE.data as bigint) ?? BigInt(0);
   USDE_ALLOWANCE.base = (SC_USDE_ALLOWACE_BASE.data as bigint) ?? BigInt(0);
@@ -160,6 +174,11 @@ const SwapCard = () => {
   }) ?? { lzTokenFee: BigInt(0), nativeFee: BigInt(0) };
   composedMessage = (SC_COMPOSED_MESSAGE.data as string) ?? "";
   useEffect(() => {
+
+    const rejoinBatchCondition = 
+        activeFunds[0] != currentBatch && activeFunds[1] > BigInt(0) ? true : false; 
+    setRejoinBatch(rejoinBatchCondition);
+
     const userTicketCalc =
       USER_TICKET[1] > BigInt(0) ? Number(USER_TICKET[1] / TP) : 0;
     setTicketsPrice(Number(formatEther(TP.toString())));
@@ -177,6 +196,25 @@ const SwapCard = () => {
     if (account.chainId === baseSepolia.id) setFromChain("base");
 
   }, [buyAmount, TP, USDE_BALANCE, fromChain, account.chainId]);
+
+  const handleRejoin = async () => {
+    writeContractAsync({
+      address: baseSepoliaContract.FORTU_POOL.address as `0x${string}`,
+      abi: baseSepoliaContract.FORTU_POOL.abi,
+      functionName: "rejoinBacth (0x29c5ce08)",
+      args: [activeFunds[0]],
+    }).then(() => {
+      toast({
+        title: "Success Migration",
+        description: "Your funds has been successfully migrated",
+      })
+    }).catch((err) => {
+      toast({
+        title: "Failed Migration",
+        description: "Your funds failed to migrated",
+      })
+    })
+  }
 
   const handleBuyTicket = async () => {
     const amountInWei = parseEther(buyAmount.toString());
@@ -304,129 +342,176 @@ const SwapCard = () => {
         break;
     }
   };
-
+  
   return (
     <div className="swap__element flex-[0.3] relative">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>
-            <div className="flex justify-between items-center">
-              <div>Buy Tickets</div>
-              <div className="text-sm flex items-center">
-                <div className="mr-2">Ticket Owned : {userTicket}</div>
-                <Ticket className="inline-block my-auto" size={15} />
+      { !rejoinBatch && (
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>
+              <div className="flex justify-between items-center">
+                <div>Buy Tickets</div>
+                <div className="text-sm flex items-center">
+                  <div className="mr-2">Ticket Owned : {userTicket}</div>
+                  <Ticket className="inline-block my-auto" size={15} />
+                </div>
               </div>
-            </div>
-          </CardTitle>
-          <CardDescription>
-            Buy Tickets by deposit your money into pool
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="input__wrapper">
-            <div className="flex justify-between items-center mb-3">
-              <div className="flex items-center">
-                <img
-                  src="https://app.ethena.fi/shared/usde.svg"
-                  style={{ width: "25px", height: "25px" }}
-                  alt="usde"
+            </CardTitle>
+            <CardDescription>
+              Buy Tickets by deposit your money into pool
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="input__wrapper">
+              <div className="flex justify-between items-center mb-3">
+                <div className="flex items-center">
+                  <img
+                    src="https://app.ethena.fi/shared/usde.svg"
+                    style={{ width: "25px", height: "25px" }}
+                    alt="usde"
+                  />
+                  <span className="ml-2">USDe</span>
+                </div>
+                <div className="flex items-center">
+                  <Badge className="mr-2">{ticketPrice} USDe / Ticket</Badge>
+                </div>
+              </div>
+              <div className="flex w-full justify-between items-center space-x-2">
+                <Input
+                  type="number"
+                  value={buyAmount}
+                  placeholder="Amount"
+                  onChange={(e) => {
+                    {
+                      setTickets(
+                        Math.trunc(Number(e.target.value ?? 0) / ticketPrice)
+                      );
+                      setBuyAmount(Number(e.target.value ?? 0));
+                    }
+                  }}
                 />
-                <span className="ml-2">USDe</span>
+                <Select
+                  value={fromChain}
+                  onValueChange={(e: string) => {
+                    setFromChain(e);
+                    handleSwitchChain(e);
+                  }}
+                >
+                  <SelectTrigger className="w-[65px]">
+                    <SelectValue placeholder="" className="mr-1" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="uni">
+                        <img
+                          src="https://unichain-sepolia.blockscout.com/assets/configs/network_icon.svg"
+                          style={{ width: "20px", height: "20px" }}
+                        />
+                      </SelectItem>
+                      <SelectItem value="base">
+                        <img
+                          src="https://sepolia.basescan.org/assets/basesepolia/images/svg/logos/chain-light.svg?v=24.11.2.0"
+                          style={{ width: "20px", height: "20px" }}
+                        />
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </div>
-              <div className="flex items-center">
-                <Badge className="mr-2">{ticketPrice} USDe / Ticket</Badge>
+              <div className="inline-block pr-3 font-sans ml-1 text-sm">
+                Balance : {toCurrency(usdeBalance)}{" "}
+                <span
+                  className="text-blue-700 ml-2 cursor-pointer"
+                  onClick={handleMaxButton}
+                >
+                  Max
+                </span>
               </div>
-            </div>
-            <div className="flex w-full justify-between items-center space-x-2">
-              <Input
-                type="number"
-                value={buyAmount}
-                placeholder="Amount"
-                onChange={(e) => {
-                  {
-                    setTickets(
-                      Math.trunc(Number(e.target.value ?? 0) / ticketPrice)
-                    );
-                    setBuyAmount(Number(e.target.value ?? 0));
-                  }
-                }}
-              />
-              <Select
-                value={fromChain}
-                onValueChange={(e: string) => {
-                  setFromChain(e);
-                  handleSwitchChain(e);
-                }}
-              >
-                <SelectTrigger className="w-[65px]">
-                  <SelectValue placeholder="" className="mr-1" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="uni">
-                      <img
-                        src="https://unichain-sepolia.blockscout.com/assets/configs/network_icon.svg"
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                    </SelectItem>
-                    <SelectItem value="base">
-                      <img
-                        src="https://sepolia.basescan.org/assets/basesepolia/images/svg/logos/chain-light.svg?v=24.11.2.0"
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="inline-block pr-3 font-sans ml-1 text-sm">
-              Balance : {toCurrency(usdeBalance)}{" "}
-              <span
-                className="text-blue-700 ml-2 cursor-pointer"
-                onClick={handleMaxButton}
-              >
-                Max
-              </span>
-            </div>
 
-            <div className="pt-5 mx-auto w-full">
-              <span className="font-semibold block text-center w-full text-2xl">
-                Total Tickets
-              </span>
-              <p className="whitespace-pre-wrap text-6xl text-center font-medium tracking-tighter text-black dark:text-white">
-                {tickets ? (
-                  <NumberTicker value={tickets} />
-                ) : (
-                  <span className="mr-1">0</span>
-                )}
-              </p>
+              <div className="pt-5 mx-auto w-full">
+                <span className="font-semibold block text-center w-full text-2xl">
+                  Total Tickets
+                </span>
+                <p className="whitespace-pre-wrap text-6xl text-center font-medium tracking-tighter text-black dark:text-white">
+                  {tickets ? (
+                    <NumberTicker value={tickets} />
+                  ) : (
+                    <span className="mr-1">0</span>
+                  )}
+                </p>
+              </div>
             </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          {approveAmount < parseEther(buyAmount.toString()) ? (
-            <ShimmerButton
-              className="shadow-2xl w-full"
-              background="#6366f1"
-              onClick={handleApprove}
-            >
-              <span className="whitespace-pre-wrap text-center text-md font-semibold font-medium leading-none tracking-tight text-white dark:from-white dark:to-blue-600/10 lg:text-md">
-                Approve
-              </span>
-            </ShimmerButton>
-          ) : (
-            <ShimmerButton
-              className="shadow-2xl w-full"
-              background="#6366f1"
-              onClick={handleBuyTicket}
-            >
-              <span className="whitespace-pre-wrap text-center text-md font-semibold font-medium leading-none tracking-tight text-white dark:from-white dark:to-blue-600/10 lg:text-md">
-                Buy
-              </span>
-            </ShimmerButton>
-          )}
-        </CardFooter>
-      </Card>
+          </CardContent>
+          <CardFooter>
+            {approveAmount < parseEther(buyAmount.toString()) ? (
+              <ShimmerButton
+                className="shadow-2xl w-full"
+                background="#6366f1"
+                onClick={handleApprove}
+              >
+                <span className="whitespace-pre-wrap text-center text-md font-semibold font-medium leading-none tracking-tight text-white dark:from-white dark:to-blue-600/10 lg:text-md">
+                  Approve
+                </span>
+              </ShimmerButton>
+            ) : (
+              <ShimmerButton
+                className="shadow-2xl w-full"
+                background="#6366f1"
+                onClick={handleBuyTicket}
+              >
+                <span className="whitespace-pre-wrap text-center text-md font-semibold font-medium leading-none tracking-tight text-white dark:from-white dark:to-blue-600/10 lg:text-md">
+                  Buy
+                </span>
+              </ShimmerButton>
+            )}
+          </CardFooter>
+        </Card>
+      )}
+      { rejoinBatch && (
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>
+              <div className="flex justify-between items-center">
+                <div>Migrate Funds</div>
+                <div className="text-sm flex items-center">
+                  <div className="mr-2">Ticket Owned : {userTicket}</div>
+                  <Ticket className="inline-block my-auto" size={15} />
+                </div>
+              </div>
+            </CardTitle>
+            <CardDescription>
+              Migrate your funds to follow the current batch
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="input__wrapper">
+              <div className="p-10 mx-auto w-full  flex flex-col items-center justify-center gap-4">
+                <span className="font-semibold block text-center w-full text-2xl">
+                  Last Batch Funds
+                </span>
+                <p className="whitespace-pre-wrap text-6xl text-center font-medium tracking-tighter text-black dark:text-white">
+                  {tickets ? (
+                    <NumberTicker value={tickets} />
+                  ) : (
+                    <span className="mr-1">0</span>
+                  )}
+                </p>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+              <ShimmerButton
+                className="shadow-2xl w-full"
+                background="#6366f1"
+                onClick={handleRejoin}
+              >
+                <span className="whitespace-pre-wrap text-center text-md font-medium leading-none tracking-tight text-white dark:from-white dark:to-blue-600/10 lg:text-md">
+                  Migrate
+                </span>
+              </ShimmerButton>
+          </CardFooter>
+        </Card>
+      )}
       <BorderBeam size={250} duration={12} delay={9} className="rounded-md" />
     </div>
   );
